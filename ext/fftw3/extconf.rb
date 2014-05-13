38d9c59b368306bcf5cd255532dc777824fcfba0
Magdalen Berns <m.berns@thismagpie.com>
2014-05-13 01:05:17 +0100

    EXTCONF: fix style and nmatrix header searches

--- ext/fftw3/extconf.rb
+++ ext/fftw3/extconf.rb
@@ -1,22 +1,53 @@
 require "mkmf"
 require "rubygems"
 
-nmatrix_dir = File.dirname(Gem.find_files("nmatrix.h").first) rescue $sitearchdir
-dir_config('nmatrix', nmatrix_dir, nmatrix_dir)
-dir_config('fftw3/include','/usr/include')
-
-if ( ! have_header("nmatrix.h") && have_header("nmatrix_config.h") ) then
-   print <<-EOS
-   ** configure error **
-
-   Header nmatrix.h or nmatrix_config.h was not found. If you have these files in
-   /nmatrixdir/include, try the following:
-
-   % ruby extconf.rb --with-nmatrix-include=/nmatrix-dir/include
-
-EOS
-   exit(-1)
+module FFTW
+  class Version
+    def initialize(str)
+      @str = str
+      @ary = str.split(".").collect { |elm| elm.to_i }
+    end
+    def to_s; @str; end
+    def inspect; @str; end
+    def >=(ver)
+      ary2 = ver.split(".").collect { |elm| elm.to_i }
+      if @ary[0] > ary2[0]; return true; end
+      if @ary[0] < ary2[0]; return false; end
+      if @ary[1] > ary2[1]; return true; end
+      if @ary[1] < ary2[1]; return false; end
+      if @ary.size < ary2.size; return false; end
+      if @ary.size == 3 and ary2.size == 3
+        if @ary[2] < ary2[2]; return false; end
+      end
+      return true
+    end
+    def <(ver)
+      ary2 = ver.split(".").collect { |elm| elm.to_i }
+      if @ary[0] >= ary2[0]; return false; end
+      if @ary[0] >= ary2[0]; return false; end
+      return true
+    end
+  end
 end
+
+nmatrix_config = dir_config('../nmatrix',$sitearchdir,$sitearchdir)
+begin
+  require 'rubygems'
+  nm_gemspec=Gem::Specification.find_by_path('nmatrix.h')
+  if nm_gemspec
+    nmatrix_config = File.join(nm_gemspec.full_gem_path, nm_gemspec.require_path)
+    $CPPFLAGS = " -I#{nmatrix_config} "+$CPPFLAGS
+  end
+rescue LoadError
+end
+have_nmatrix_h = have_header("nmatrix.h")
+if nmatrix_config
+  if RUBY_PLATFORM =~ /cygwin|mingw/
+    have_library("nmatrix")
+  end
+end
+
+dir_config('fftw3/include','/usr/include',$sitearchdir)
 
 if ( ! have_header("fftw3.h") && have_library("fftw3") ) then
    print <<EOS
