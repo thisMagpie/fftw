1cb17838cb4f53777f1a0474f7fbd213c5d73247
Magdalen Berns <m.berns@thismagpie.com>
2014-05-20 13:55:30 +0100

    fftw3.rb: only use to require libraries

--- lib/fftw3.rb
+++ lib/fftw3.rb
@@ -2,30 +2,10 @@ require 'rubygems'
 require 'nmatrix'
 require 'fftw3/fftw3'
 
-module FFTW3
-  def self.fft(*args)
-    args.insert(1, -1)
-    fft_raw(*args)
-  end
+if File.exist?("lib/fftw3.so")
+  require "fftw3/fftw3.so"
+end
 
-  def self.ifft(*args)
-    args.insert(1, 1)
-    fft_raw(*args)
-  end
-
-  # source http://www.gregfjohnson.com/fftruby/
-  def vec_fft(vec)
-    return if vec.size <= 1
-
-    even = fft(NMatrix.new(vec.size / 2) { |i| vec[2 * i] })
-    odd  = fft(NMatrix.new(vec.size / 2) { |i| vec[2 * i + 1] })
-
-    fft_even.concat(fft_even)
-    fft_odd.concat(fft_odd)
-
-  end
-
-  module_function :fft
-  module_function :ifft
-  module_function :vec_fft
+if File.exist?("ext/fftw3.o")
+  require "fftw3/fftw3.o"
 end
