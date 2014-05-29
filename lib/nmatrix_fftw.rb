8a11bfc5c81621c20705818482ebdd6cd3129ee4
Magdalen Berns <m.berns@thismagpie.com>
2014-05-29 20:23:02 +0100

    lib: rename for nmatrix fftw

--- lib/nmatrix_fftw.rb
+++ lib/nmatrix_fftw.rb
@@ -2,10 +2,10 @@ require 'rubygems'
 require 'nmatrix'
 require 'fftw3/nmatrix-fftw'
 
-if File.exist?("lib/nmatrix-fftw.so")
-  require "fftw3/nmatrix-fftw.so"
+if File.exist?("lib/nmatrix_fftw.so")
+  require "fftw3/nmatrix_fftw.so"
 end
 
-if File.exist?("ext/nmatrix-fftw.o")
-  require "fftw3/nmatrix-fftw.o"
+if File.exist?("ext/nmatrix_fftw.o")
+  require "fftw3/nmatrix_fftw.o"
 end
