22bdfa0be1eed7adad5da0254dce263c089b08ec
Magdalen Berns <m.berns@thismagpie.com>
2014-05-30 22:22:19 +0100

    lib/ simplify requires

--- lib/nmatrix_fftw.rb
+++ lib/nmatrix_fftw.rb
@@ -3,9 +3,9 @@ require 'nmatrix'
 require 'fftw3/nmatrix-fftw'
 
 if File.exist?("lib/nmatrix_fftw.so")
-  require "fftw3/nmatrix_fftw.so"
+  require "nmatrix_fftw.so"
 end
 
 if File.exist?("ext/nmatrix_fftw.o")
-  require "fftw3/nmatrix_fftw.o"
+  require "nmatrix_fftw.o"
 end
