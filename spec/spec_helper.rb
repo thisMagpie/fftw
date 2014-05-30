ff0285b83501d2855c8da95b1307a650e50aa91e
Magdalen Berns <m.berns@thismagpie.com>
2014-05-31 00:02:05 +0100

    remove all but bare minimum from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,7 @@
 require 'bundler/setup'
 Bundler.setup
-
 require 'nmatrix-fftw'
-require 'nmatrix'
 
 RSpec.configure do |config|
-    config.expect_with :rspec, :stdlib,:fftw
+    config.expect_with :rspec, :stdlib
 end
