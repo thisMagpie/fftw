dbec309833062f1c84ed987e0bd99003d15cf752
Magdalen Berns <m.berns@thismagpie.com>
2014-06-05 00:41:52 +0100

    Fix problem finding fftw
    works with ruby ext/nmatrix-fftw/extconf now

--- ext/nmatrix-fftw/extconf.rb
+++ ext/nmatrix-fftw/extconf.rb
@@ -1,80 +1,6 @@
 require 'mkmf'
 require 'rubygems'
-
-# Function derived from NArray's extconf.rb.
-def have_type(type, header=nil)
-  printf "checking for %s... ", type
-  STDOUT.flush
-
-  src = <<"SRC"
-#include <ruby.h>
-SRC
-
-
-  src << <<"SRC" unless header.nil?
-#include <#{header}>
-SRC
-
-  r = try_link(src + <<"SRC")
-  int main() { return 0; }
-  int t() { #{type} a; return 0; }
-SRC
-
-  unless r
-    print "no\n"
-    return false
-  end
-
-  $defs.push(format("-DHAVE_%s", type.upcase))
-
-  print "yes\n"
-
-  return true
-end
-
-def nmatrix_fftw_config()
-  print("checking nmatrix_fftw cflags... ")
-  IO.popen('#{NMatrix_FFTW_CONFIG} --cflags') do |f|
-    cflags = f.gets.chomp
-    puts(cflags)
-    $CFLAGS += ' ' + cflags
-  end
-
-  IO.popen('#{NMatrix_FFTW_CONFIG} --libs') do |f|
-    libs = f.gets.chomp
-    dir_config("cblas")
-    dir_config("atlas")
-    if have_library('cblas') and have_library('atlas')
-      libs.gsub!('-lfftw3cblas', '-lcblas -latlas')
-      $LOCAL_LIBS += ' ' + libs.gsub(' -lfftw3cblas', '')
-      print("checking fftw3 libs... ")
-      puts(libs)
-    else
-      print("checking fftw3 libs... ")
-      puts(libs)
-      $LOCAL_LIBS += " " + libs
-    end
-  end
-end
-# Function derived from NArray's extconf.rb.
-def create_conf_h(file) #:nodoc:
-  print "creating #{file}\n"
-  File.open(file, 'w') do |hfile|
-    header_guard = file.upcase.sub(/\s|\./, '_')
-
-    hfile.puts "#ifndef #{header_guard}"
-    hfile.puts "#define #{header_guard}"
-    hfile.puts
-
-    for line in $defs
-      line =~ /^-D(.*)/
-      hfile.printf "#define %s 1\n", $1
-    end
-
-    hfile.puts
-    hfile.puts "#endif"
-  end
-end
+require 'nmatrix'
 
 nmatrix_config = dir_config('../nmatrix',$sitearchdir,$sitearchdir)
 begin
@@ -103,21 +29,20 @@ if ( ! have_header("fftw3.h") && have_library("fftw3") ) then
    If you have the library installed under /fftw3-dir (that is, fftw3.h is
    in /fftw3-dir/include and the library in /fftw3-dir/lib/),
    try the following:
-
    % ruby extconf.rb --with-fftw3-dir=/fftw3-dir
 
    Alternatively, you can specify the two directory separately
    with --include-dir and --fftw3-dir
 EOS
-   exit(-1)
+  exit(-1)
 end
 
 if have_library("fftw3f")
-  $CFLAGS += ' -DFFTW3_HAS_SINGLE_SUPPORT -Wall -I../include'
+ $CFLAGS += ' -DFFTW3_HAS_SINGLE_SUPPORT -Wall -I../include'
 end
 
-if /cygwin|mingw/ =~ RUBY_PLATFORM
+if /cygwin|mingw/ =~RUBY_PLATFORM
    have_library("nmatrix") || raise("ERROR: nmatrix library is not found")
 end
 
-create_makefile("nmatrix_fftw")
+create_makefile("fftw")
