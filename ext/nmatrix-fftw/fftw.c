1be578021d5b1878cf148ac98afe48695426163b
Magdalen Berns <m.berns@thismagpie.com>
2014-06-09 20:49:09 +0100

    Ext: Add module initialisation

--- ext/nmatrix-fftw/fftw.c
+++ ext/nmatrix-fftw/fftw.c
@@ -1,9 +1,9 @@
 #include <fftw3.h>
 #include <ruby.h>
 
-static VALUE rb_cFFTW3;
+VALUE mFFTW;
 
 void Init_fftw()
 {
-  rb_cFFTW = rb_define_class("FFTW", rb_cObject);
+  rb_define_module("FFTW");
 }
