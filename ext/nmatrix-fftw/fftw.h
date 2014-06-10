a7e4c3392987c2bee4ae3a716b938a0bc687bf62
Magdalen Berns <m.berns@thismagpie.com>
2014-06-10 03:32:00 +0100

    Tidy up headers a bit

--- ext/nmatrix-fftw/fftw.h
+++ ext/nmatrix-fftw/fftw.h
@@ -1,9 +1,11 @@
 #ifndef NMatrixFFTW
 #define NMatrixFFTW
 
-#include <ruby.h>;
-#include <fftw3.h>;
+#include <ruby.h>
+#include <fftw3.h>
 
 extern VALUE mFFTW;
 
+#define FFTW_FORWARD (-1)
+#define FFTW_BACKWARD (+1)
 #endif
