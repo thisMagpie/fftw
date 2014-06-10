57b8e96503fde02ce886cf3a6f0ffefb311cdb75
Magdalen Berns <m.berns@thismagpie.com>
2014-06-10 05:38:50 +0100

    extconf: remove broken nmatrix references

--- ext/nmatrix-fftw/extconf.rb
+++ ext/nmatrix-fftw/extconf.rb
@@ -1,25 +1,7 @@
 require 'mkmf'
 require 'rubygems'
-require 'nmatrix'
-
-nmatrix_config = dir_config('../nmatrix',$sitearchdir,$sitearchdir)
-begin
-  nm_gemspec=Gem::Specification.find_by_path('nmatrix.h')
-  if nm_gemspec
-    nmatrix_config = File.join(nm_gemspec.full_gem_path, nm_gemspec.require_path)
-    $CPPFLAGS = " -I#{nmatrix_config} "+$CPPFLAGS
-  end
-rescue LoadError
-end
-have_nmatrix_h = have_header("nmatrix.h")
-if nmatrix_config
-  if RUBY_PLATFORM =~ /cygwin|mingw/
-    have_library("nmatrix")
-  end
-end
 
 dir_config('include','/usr/include',$sitearchdir)
-
 if ( ! have_header("fftw3.h") && have_library("fftw3") ) then
    print <<EOS
    ** configure error **
@@ -37,11 +19,7 @@ EOS
 end
 
 if have_library("fftw3f")
- $CFLAGS += ' -DFFTW3_HAS_SINGLE_SUPPORT -Wall -I../include'
-end
-
-if /cygwin|mingw/ =~RUBY_PLATFORM
-   have_library("nmatrix") || raise("ERROR: nmatrix library is not found")
+ $CFLAGS += ' -DFFTW3_HAS_SINGLE_SUPPORT -Wall -I../usr/include'
 end
 
 create_makefile("nmatrix-fftw")
