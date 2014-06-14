d0b30296bf7903c199aa0ce7fbf13acf7728baef
Magdalen Berns <m.berns@thismagpie.com>
2014-06-14 02:56:06 +0100

    Ext: define fftw class and method

--- ext/nmatrix-fftw/fftw.c
+++ ext/nmatrix-fftw/fftw.c
@@ -1,9 +1,11 @@
 #include <fftw3.h>
 #include <ruby.h>
 
-VALUE mFFTW;
+VALUE cNMatrixFFTW;
+VALUE rb_fftw_1d(int argc, VALUE* argv, VALUE self);
 
-void Init_fftw()
+void Init_nmatrix_fftw()
 {
-  rb_define_module("FFTW");
+  cNMatrixFFTW = rb_define_class("NMatrixFFTW", rb_cObject);
+  rb_define_method(cNMatrixFFTW, "fftw_1d", rb_fftw_1d, -1);
 }
