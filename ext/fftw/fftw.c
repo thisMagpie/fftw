1c9f5e3ab6d57ffe5b6c1195d784e4fdb7576e7a
Magdalen Berns <m.berns@thismagpie.com>
2014-06-30 20:28:23 +0100

    fftw.c add basics for a r2r method

--- ext/fftw/fftw.c
+++ ext/fftw/fftw.c
@@ -1,11 +1,24 @@
 #include <fftw3.h>
 #include <ruby.h>
-
+#include <stdio.h>
 VALUE mFFTW;
 
-static VALUE fftw(int argc, VALUE* argv, VALUE self);
+# Define Real To Real Transform Method
+# TODO to be tested
+# Once this is done this should be able to take nmatrix objects of real data
+# so that the transformed array can be returned an nmatrix object which holds
+# a real to real transform in an nmatrix array
+static VALUE fftw_r2r(VALUE self, VALUE r2r)
+{
+   VALUE p;
+   rb_need_block();
+   p = rb_block_proc();
+   rb_funcall(self, rb_intern("define_method"), 2, r2r, p);
 
+   return Qnil;
+}
 void Init_fftw()
 {
   mFFTW = rb_define_module("FFTW");
+  rb_define_singleton_method(mFFTW,"r2r",fftw_r2r,1);
 }
