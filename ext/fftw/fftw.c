06fb08e60c48c8b07dfa4409f324fa21cb6910f2
Magdalen Berns <m.berns@thismagpie.com>
2014-06-30 20:39:32 +0100

    ext: use the same funtion name as matlab

--- ext/fftw/fftw.c
+++ ext/fftw/fftw.c
@@ -1,24 +1,25 @@
 #include <fftw3.h>
 #include <ruby.h>
 #include <stdio.h>
-VALUE mFFTW;
 
-# Define Real To Real Transform Method
-# TODO to be tested
-# Once this is done this should be able to take nmatrix objects of real data
-# so that the transformed array can be returned an nmatrix object which holds
-# a real to real transform in an nmatrix array
-static VALUE fftw_r2r(VALUE self, VALUE r2r)
+VALUE mFFTW;
+/**
+ * Define Real To Real Transform Method
+ * TODO to be tested
+ * Once this is done this should be able to take nmatrix objects of real data
+ * so that the transformed array can be returned an nmatrix object which holds
+ * a real to real transform in an nmatrix array
+ */
+static VALUE fftw_fft(VALUE self, VALUE r2r)
 {
    VALUE p;
    rb_need_block();
    p = rb_block_proc();
    rb_funcall(self, rb_intern("define_method"), 2, r2r, p);
-
    return Qnil;
 }
 void Init_fftw()
 {
   mFFTW = rb_define_module("FFTW");
-  rb_define_singleton_method(mFFTW,"r2r",fftw_r2r,1);
+  rb_define_singleton_method(mFFTW,"fft",fftw_fft,1);
 }
