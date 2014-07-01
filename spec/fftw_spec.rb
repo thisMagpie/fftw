b1860708279422a44227cfb60d3f0eb5f6f470f9
Magdalen Berns <m.berns@thismagpie.com>
2014-07-01 05:03:27 +0100

    rspec: add r2r describe

--- spec/fftw_spec.rb
+++ spec/fftw_spec.rb
@@ -3,9 +3,9 @@ require 'fftw'
 require 'nmatrix'
 require 'rspec/expectations'
 
-describe FFTW do
+describe FFTW, "r2r" do
   it "creates an nmatrix object" do
-    n = NMatrix.new([2,2], [0,1,2,3], dtype: :int64)e
+    n = NMatrix.new([2,2], [0,1,2,3], dtype: :int64)
     expect(n.shape).to eq([2,2])
     expect(n.entries).to eq([0,1,2,3])
     expect(n.dtype).to eq(:int64)
