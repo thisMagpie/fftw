9e49e9fcd399328d767f397d29374b4ae8a3bc11
Magdalen Berns <m.berns@thismagpie.com>
2014-07-01 04:33:58 +0100

    specs: check fftw arguments

--- spec/fftw_spec.rb
+++ spec/fftw_spec.rb
@@ -1,14 +1,17 @@
 require 'spec_helper'
 require 'fftw'
 require 'nmatrix'
+require 'rspec/expectations'
 
 describe FFTW do
-  it "creates a matrix with the new constructor" do
-    n = NMatrix.new([2,2], [0,1,2,3], dtype: :int64)
+  it "creates an nmatrix object" do
+    n = NMatrix.new([2,2], [0,1,2,3], dtype: :int64)e
     expect(n.shape).to eq([2,2])
     expect(n.entries).to eq([0,1,2,3])
     expect(n.dtype).to eq(:int64)
   end
+  it "creates an fftw object and checks its arguments" do
+    fftw = FFTW.new() #=> create a new fftw unstance
+    #expect { |fftw| 0.tap(fftw) }.to yield_with_args(0)
+  end
 end
-
-
