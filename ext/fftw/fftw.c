3ebcfbd5a986994553f21f8c0adb2b1673c5743e
Magdalen Berns <m.berns@thismagpie.com>
2014-07-02 09:48:12 +0100

    [ ext | fftw.c ]  fix some warnings

--- ext/fftw/fftw.c
+++ ext/fftw/fftw.c
@@ -18,27 +18,32 @@ VALUE mFFTW;
  * Y = fft(X,[],dim)
  * Y = fft(X,n,dim)
  */
-static VALUE fftw_r2r(VALUE self, VALUE r2r)
+static VALUE fftw_r2r(double *r2r, int m)
 {
-  VALUE p;
   double *in, *out;
-  in = calloc(r2r, sizeof(double));
-  rb_funcall(self, rb_intern("define_method"), 2, r2r, rb_block_proc());
-  int i,j;
-  for(i = 0; i < 10; i++)
+  int n;
+  printf("%d ",&m);
+  /* get the size of the array*/
+  size_t size = (sizeof r2r / sizeof r2r[0]);
+
+  int i, j;
+
+  for(i = 0; i < m; i++)
   {
-    for(j = 0; j < 10; j++)
+
+  in =  malloc(sizeof(r2r)) ;
+    for(j = 0; j < m; j++)
     {
-      out[i] += NUM2DBL(rb_ary_entry(r2r, (i * 10) + j));
+      printf("Enter in value\n");
+      out[i] = in[(i * m) + j];
     }
-    out[i] /= 10;
-    prinf(out[i]);
+    out[i] /= m;;
   }
-  return &out;
+  return out[m-1];
 }
 
 void Init_fftw()
 {
   mFFTW = rb_define_module("FFTW");
-  rb_define_singleton_method(mFFTW,"fftw_r2r",fftw_r2r,1);
+  rb_define_singleton_method(mFFTW,"r2r",fftw_r2r,1);
 }
