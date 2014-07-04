651bd75d70dcd5d39e439531e33039b0534cc572
Magdalen Berns <m.berns@thismagpie.com>
2014-07-05 00:07:43 +0100

    find nmatrix headers

--- ext/fftw/extconf.rb
+++ ext/fftw/extconf.rb
@@ -3,15 +3,19 @@ require 'colorize'
 
 LIBDIR = RbConfig::CONFIG['libdir']
 INCLUDEDIR = RbConfig::CONFIG['includedir']
-HEADER_DIRS = ['/opt/local/include',
-               '/usr/local/include',
+NMATRIX_DIR = ENV['GEM_HOME'] + '/gems/nmatrix-0.1.0.rc3/'
+NMATRIX_LIBDIR = NMATRIX_DIR + 'lib/nmatrix/'
+NMATRIX_INCLUDEDIR = NMATRIX_DIR + 'ext/nmatrix/'
+
+HEADER_DIRS = ['/usr/local/include',
                 INCLUDEDIR,
                '/usr/include',
-               '/usr/include/atlas']
+               '/usr/include/atlas',
+               NMATRIX_INCLUDEDIR
+              ]
+LIB_DIRS = [LIBDIR,NMATRIX_LIBDIR]
 
-puts "Library directory:" + LIBDIR.yellow
-puts "Include directory:" + INCLUDEDIR.yellow
-puts "Headers directory:" + "#{HEADER_DIRS}".yellow.to_s
+dir_config('fftw',HEADER_DIRS,LIBDIR)
 
 if /mingw/ =~ RUBY_PLATFORM then
   FFTW_CONFIG = "sh fftw"
@@ -19,54 +23,43 @@ else
   FFTW_CONFIG = "fftw"
 end
 
-def fftw_config()
-  print("checking fftw_cflags... ")
-  IO.popen("#{FFTW_CONFIG} --cflags") do |f|
-    cflags = f.gets.chomp
-    puts(cflags)
-    $CFLAGS += " " + cflags
-  end
-  # This is for nmatrix stuff late on TODO
-  IO.popen("#{FFTW_CONFIG} --libs") do |f|
-    libs = f.gets.chomp
+# set up configurtion
+puts "Library directory:" + LIBDIR.green
+puts "Include directory:" + INCLUDEDIR.green
+puts "Headers directory:" + "#{HEADER_DIRS}".green.to_s
+puts '#{sitearchdir}'.red
+
+if ( ! have_header("fftw3.h") && have_library("fftw3") ) then
+  print <<-EOS
+  ** configure error **
+
+  Header fftw.h or the compiled fftw library is not found.
+  If you have the library installed under /fftw-dir (that is, fftw3.h is
+  in /fftw dir/include and the library in /fftw-dir/lib/),
+  try the following:
+  % ruby extconf.rb --with-fftw-dir=/fftw-dir
+
+  Alternatively, you can specify the two directory separately
+  with --include-dir and --fftw-dir
+  EOS
+  exit(-1)
+end
+
+if have_library("fftw3f")
+  $CFLAGS = [" -DFFTW3_HAS_SINGLE_SUPPORT -Wall -I #{INCLUDEDIR}"].join(" ")
+end
+
+require 'rubygems'
+nm_gemspec = Gem::Specification.find_by_path('nmatrix.h')
+if nm_gemspec then
+  if have_header('nmatrix.h')
     dir_config("cblas")
     dir_config("atlas")
     if have_library("cblas") and have_library("atlas")
-      libs.gsub!("-lfftwfblas", "-lcblas -latlas")
-      $LOCAL_LIBS += " " + libs.gsub(" -lfftwcblas", "")
-      print("checking fftw libs... ")
-      puts(libs)
+      puts "found cblas and atlas".green
     end
   end
-
-  if have_library("fftw3f")
-    $CFLAGS = [" -DFFTW3_HAS_SINGLE_SUPPORT -Wall -I #{INCLUDEDIR}"].join(" ")
-  end
-  common_flags ="-lfftw3 -Wall -Werror=return-type"
-  $CFLAGS   = [common_flags,$CFLAGS].join(" ")
-  $CXXFLAGS = [common_flags,$CXXFLAGS].join(" ")
-  $CPPFLAGS = [common_flags,$CPPFLAGS].join(" ")
-
-  # set up configurtion
-  dir_config('fftw',HEADER_DIRS,LIBDIR)
-
-  if ( ! have_header("fftw3.h") && have_library("fftw3") ) then
-    print <<-EOS
-    ** configure error **
-
-    Header fftw.h or the compiled fftw library is not found.
-    If you have the library installed under /fftw-dir (that is, fftw3.h is
-    in /fftw dir/include and the library in /fftw-dir/lib/),
-    try the following:
-    % ruby extconf.rb --with-fftw-dir=/fftw-dir
-
-    Alternatively, you can specify the two directory separately
-    with --include-dir and --fftw-dir
-    EOS
-    exit(-1)
-  end
-# TODO FFTW for i866 or x86-64 Computers
-#'--enable-float  --enable-threads  --enable-sse'
 end
+
 
 create_makefile("fftw/fftw")
