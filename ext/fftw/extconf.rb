0fad33b0149e1ccf62ee27b66bc98edc7a70f4dc
Magdalen Berns <m.berns@thismagpie.com>
2014-07-07 14:43:42 +0100

    exconf: fix some more errors

--- ext/fftw/extconf.rb
+++ ext/fftw/extconf.rb
@@ -19,8 +19,18 @@ puts "####################".colorize(:color => :cyan,
 # LIBS    == fftw_libdir
 #
 ###############################################################################
-fftw_libdir    = RbConfig::CONFIG['libdir']
-fftw_incdir    = RbConfig::CONFIG['includedir']
+fftw_libdir = RbConfig::CONFIG['libdir']
+fftw_incdir = RbConfig::CONFIG['includedir']
+
+def create_conf_h(file)
+  print "creating #{file}\n"
+  hfile = open(file, "w")
+  for line in $defs
+    line =~ /^-D(.*)/
+    hfile.printf "#define %s 1\n", $1
+  end
+  hfile.close
+end
 ###############################################################################
 #
 # README output messages for debugging.
@@ -30,7 +40,12 @@ puts "Include directories:".white
 puts "#{fftw_incdir}".green
 puts "Library directories:".white
 puts "#{fftw_libdir}".green
-
+have_type("u_int8_t", fftw_incdir)
+have_type("uint8_t", fftw_incdir)
+have_type("int16_t", fftw_incdir)
+have_type("int32_t", fftw_incdir)
+have_type("u_int32_t", fftw_incdir)
+have_type("uint32_t", fftw_incdir)
 ###############################################################################
 #
 # Make sure CXX is set to g++ and not clang or gcc by setting its value
@@ -41,30 +56,24 @@ def gplusplus_version
   major = cxxvar.call('__GNUC__')
   minor = cxxvar.call('__GNUC_MINOR__')
   patch = cxxvar.call('__GNUC_PATCHLEVEL__')
-
-  raise("unable to determine g++ version (match to get version was nil)") if major.nil? || minor.nil? || patch.nil?
-
-  "#{major}.#{minor}.#{patch}"
-end
-
-if CONFIG['CXX'] == 'clang++'
-  $CPP_STANDARD = 'c++11'
-  $CPP_FLAGS = '-std=c++11'
-  $CXX_FLAGS = '-stc=gnu++11'
-  puts "using C++ standard... #{$CPP_STANDARD}".cyan
-else
-  version = gplusplus_version
-  if version < '4.3.0' && CONFIG['CXX'] == 'g++'  # see if we can find a newer G++, unless it's been overridden by user
-    if !find_newer_gplusplus
-      raise("g++ which supports the flags -std=c++0x or -std=c++11. \
-             If you're on a Mac and using Homebrew, we recommend using \
-             mac-brew-gcc.sh to install a more recent g++.")
-    end
+  if CONFIG['CXX'] == 'clang++'
+    $CPP_STANDARD = 'c++11'
+    $CPP_FLAGS = '-std=c++11'
+    $CXX_FLAGS = '-stc=gnu++11'
+    puts "using C++ standard... #{$CPP_STANDARD}".cyan
+  else
     version = gplusplus_version
+    if version < '4.3.0' && CONFIG['CXX'] == 'g++'  # see if we can find a newer G++, unless it's been overridden by user
+      if ! find_newer_gplusplus
+        raise("g++ which supports the flags -std=c++0x or -std=c++11. \
+               If you're on a Mac and using Homebrew, we recommend using \
+               mac-brew-gcc.sh to install a more recent g++.")
+      end
+      version = gplusplus_version
+    end
+    puts "g++ reports version... " +
+    `#{CONFIG['CXX']} --version|head -n 1|cut -f 3 -d " "`.colorize(:color => :cyan ) 
   end
-  puts "g++ reports version... " +
-  `#{CONFIG['CXX']} --version|head -n 1|cut -f 3 -d " "`.colorize(:color => :cyan )
-
 end
 
 puts "Searching for cblas and atlas...".white
@@ -81,21 +90,21 @@ else
 end
 
 puts "Searching for NMatrix...".white
-if have_library('nmatrix')
-  puts "Library directories for NMatrix: Found!".colorize(:color => :green,
-                                                          :background => :white)
+nmatrix_config = dir_config('nmatrix','ext/nmatrix','lib/nmatrix')
+if nmatrix_config then
+  puts "nmatrix_config found!".green
+  unless have_library('nmatrix_config')
+    puts "Library directories for NMatrix: Not Found!".colorize(:color => :red,
+                                                                :background => :white)
+  end
+  unless find_header('nmatrix.h')
+    abort "nmatrix is missing.  please install nmatrix".red
+  end
+  unless find_header('data.h')
+    puts "Headers nmatrix.h and data.h are found!".colorize(:color => :red,
+                                                            :background => :white)
+  end
 end
-if find_header('nmatrix/nmatrix.h') and find_header('data.h')
-  puts "Headers nmatrix.h and data.h are found!".colorize(:color => :geen,
-                                                          :background => :white)
-end
-
-fftw_incdir = ['/usr/local/include',
-                fftw_incdir,
-               '/usr/include',
-               '/usr/include/atlas'
-              ]
-
 ###############################################################################
 #
 # Configuration of directory named in first argument, i.e. The arguments of
@@ -105,6 +114,11 @@ fftw_incdir = ['/usr/local/include',
 # LIBS    == fftw_libdir
 #
 ###############################################################################
+fftw_incdir = ['/usr/local/include',
+                fftw_incdir,
+               '/usr/include',
+               '/usr/include/atlas'
+              ]
 incdir, libdir = dir_config('fftw', fftw_incdir, fftw_libdir)
 
 if have_library("fftw3f") then
