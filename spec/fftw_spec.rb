37d53142f5a89cca10094c620c623d95f29381f1
Magdalen Berns <m.berns@thismagpie.com>
2014-07-07 15:06:44 +0100

    specs update with init
    parse nmatrix to fftw argument and a pure ruby function to test

--- spec/fftw_spec.rb
+++ spec/fftw_spec.rb
@@ -1,17 +1,23 @@
 require 'spec_helper'
 require 'fftw'
 require 'nmatrix'
-require 'rspec/expectations'
 
-describe FFTW, "r2r" do
+describe FFTW, "init" do
   it "creates an nmatrix object" do
     n = NMatrix.new([2,2], [0,1,2,3], dtype: :int64)
-    expect(n.shape).to eq([2,2])
-    expect(n.entries).to eq([0,1,2,3])
     expect(n.dtype).to eq(:int64)
   end
   it "creates an fftw object and checks its arguments" do
-    fftw = FFTW.new() #=> create a new fftw unstance
-    #expect { |fftw| 0.tap(fftw) }.to yield_with_args(0)
+    n = NMatrix.new([2,2], [0,1,2,3], dtype: :int64)
+    fft = FFTW.new(n) #=> create a new fftw instance
+    expect(fft.shape).to eq(n.shape)
+    expect(n.entries).to eq([0,1,2,3])
+    expect { |fft| 0.tap(fft) }.to yield_with_args(1)
+  end
+  it "Takes the inverse of a 1 kHz frequency multiplied by the number of samples per second" do
+    sample_rate = 2048 # Per Second.
+    frequency = 1000 #hz
+    max = tmax(frequency,sample_rate)
+    expect tmax.to eq(max)
   end
 end
