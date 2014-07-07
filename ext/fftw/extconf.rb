d453e1b791302a697f0e325eb67f2dee4822b102
Magdalen Berns <m.berns@thismagpie.com>
2014-07-07 08:35:25 +0100

    extconf: fix compilation error with c++11

--- ext/fftw/extconf.rb
+++ ext/fftw/extconf.rb
@@ -1,53 +1,109 @@
 require 'mkmf'
 require 'colorize'
 require 'rubygems'
-
+require 'nmatrix'
 puts "******************".colorize(:color => :light_blue,
                                    :background => :light_red)
 puts "******************".light_blue
 puts "* COMPILING FFTW *".cyan
 puts "******************".light_blue
 puts "******************".colorize(:color => :light_blue,
-                                              :background => :light_red)
+                                   :background => :light_red)
 
-# This only works if fftw3 headers are installed.
+###############################################################################
+#
+# This only works if fftw3 headers are installed
+#
+# HEADERS == fftw_incdir
+# LIBS    == fftw_libdir
+#
+###############################################################################
 fftw_libdir    = RbConfig::CONFIG['libdir']
 fftw_incdir    = RbConfig::CONFIG['includedir']
 
+###############################################################################
+#
+# Make sure CXX is set to g++ and not clang or gcc by setting its value
+#
+###############################################################################
+
+
+def gplusplus_version #:nodoc:
+  cxxvar = proc { |n| `#{CONFIG['CXX']} -E -dM - </dev/null | grep #{n}`.chomp.split(' ')[2] }
+  major = cxxvar.call('__GNUC__')
+  minor = cxxvar.call('__GNUC_MINOR__')
+  patch = cxxvar.call('__GNUC_PATCHLEVEL__')
+
+  raise("unable to determine g++ version (match to get version was nil)") if major.nil? || minor.nil? || patch.nil?
+
+  "#{major}.#{minor}.#{patch}"
+end
+
+if CONFIG['CXX'] == 'clang++'
+  $CPP_STANDARD = 'c++11'
+  $CPP_FLAGS = '-std=c++11'
+  $CXX_FLAGS = '-std=gnu++11'
+else
+  version = gplusplus_version
+  if version < '4.3.0' && CONFIG['CXX'] == 'g++'  # see if we can find a newer G++, unless it's been overridden by user
+    if !find_newer_gplusplus
+      raise("You need a version of g++ which supports -std=c++0x or -std=c++11. If you're on a Mac and using Homebrew, we recommend using mac-brew-gcc.sh to install a more recent g++.")
+    end
+    version = gplusplus_version
+  end
+
+  $CPP_STANDARD = 'c++11'
+  puts "using C++ standard... #{$CPP_STANDARD}".cyan
+  puts "g++ reports version... " + `#{CONFIG['CXX']} --version|head -n 1|cut -f 3 -d " "`.colorize(:color => :cyan )
+
+end
+
+###############################################################################
+#
 # TODO | Improve later:
 # This assumes that gems are installed in $HOME.
+#
+###############################################################################
 nmatrix_srcdir = ENV['GEM_HOME'] + '/gems/nmatrix'
-nmatrix_libdir = nmatrix_srcdir + '/lib/nmatrix'
+nmatrix_libdir = nmatrix_srcdir + '/lib'
 nmatrix_incdir = nmatrix_srcdir + '/ext/nmatrix'
 
+###############################################################################
+# 
+# Seek out nmatrix.gemspec and if it is found search for the libraries.
+###############################################################################
 nm_gemspec = Gem::Specification.find_by_path('nmatrix.h')
 if nm_gemspec then
-  puts "nmatrix.gemspec found!".green
-  puts "**********************".cyan
-  puts "Searching for cblas and atlas...".yellow
-  puts "CBLAS and ATLAS Status:".yellow
+  puts "nmatrix.gemspec found!".colorize(:color => :green,
+                                         :background => :white)
+  puts "nmatrix#{nm_gemspec.version}".colorize(:color => :black,
+                                               :background =>  :green)
+
+  puts "Searching for cblas and atlas...".white
   if have_library("cblas") and have_library("atlas")
-    puts "******".cyan # TODO stars always need to be same length as string.
-    puts "Found!".green
-    puts "******".cyan # TODO stars always need to be same length as string.
+    puts "******************************".colorize(:color => :black,
+                                                   :background => :cyan)
+    puts "CBLAS and ATLAS Status: Found!".green
+    puts "******************************".colorize(:color => :black,
+                                                   :background => :cyan)
     dir_config("cblas")
     dir_config("atlas")
   else
-    puts "Not found!".red
+    puts "CBLAS and ATLAS Status: Not found!".red
   end
 
-  # Searching for nmatrix.
-  if (have_header('nmatrix.h') && have_library('nmatrix'))
-
-    puts "NMatrix Status:".yellow
-    puts "Found!".green
-    puts "******".cyan
-  else
-    puts "NMatrix Not found!".red
+  puts "Searching for NMatrix...".white
+  if have_library('nmatrix')
+    puts "Library directories for NMatrix: Found!".colorize(:color => :geen,
+                                                            :background => :white)
+  end
+  if find_header('nmatrix.h') and find_header('data.h')
+    puts "Headers nmatrix.h and data.h are found!".colorize(:color => :geen,
+                                                            :background => :white)
   end
 end
 
-headers = ['/usr/local/include',
+fftw_incdir = ['/usr/local/include',
                 fftw_incdir,
                '/usr/include',
                '/usr/include/atlas',
@@ -55,49 +111,36 @@ headers = ['/usr/local/include',
               ]
 fftw_libdir = [ fftw_libdir, nmatrix_libdir ]
 
-puts "*********************************************************".cyan
-puts "nmatrix include directory:"
-puts "#{nmatrix_incdir}".green
-puts "nmatrix library directories:"
-puts "#{nmatrix_libdir}".green
-puts "*********************************************************".cyan
 
-dir_config('nmatrix', nmatrix_incdir, nmatrix_libdir)
+
+###############################################################################
+#
+# README output messages for debugging.
+#
+###############################################################################
+puts "nmatrix include directory:".white
+puts "#{nmatrix_incdir}".green
+puts "nmatrix library directories:".white
+puts "#{nmatrix_libdir}".green
+
+###############################################################################
+#
+# Configuration of directory named in first argument, i.e. The arguments of
+# dir_config('fftw', HEADERS, LIBS) are as follows (in this case):
+#
+# HEADERS == fftw_incdir
+# LIBS    == fftw_libdir
+#
+###############################################################################
 incdir, libdir = dir_config('fftw', fftw_incdir, fftw_libdir)
 
-if /mingw/ =~ RUBY_PLATFORM then
-  FFTW_CONFIG = "sh fftw"
-else
-  FFTW_CONFIG = "fftw"
-end
-
-# set up configurtio
-puts "Library directory:" + libdir.to_s.green
-puts "Headers directory:" + "#{incdir}".green.to_s
-
-if ( ! have_header("fftw3.h") && have_library("fftw3") ) then
-  print <<-EOS
-  ** configure error **
-
-  Header fftw.h or the compiled fftw library is not found.
-  If you have the library installed under /fftw-dir (that is, fftw3.h is
-  in /fftw dir/include and the library in /fftw-dir/lib/),
-  try the following:
-  % ruby extconf.rb --with-fftw-dir=/fftw-dir
-
-  Alternatively, you can specify the two directory separately
-  with --include-dir and --fftw-dir
-  EOS
-  exit(-1)
-end
-
-if have_library("fftw3f")
+if have_library("fftw3f") then
   $CFLAGS = [" -DFFTW3_HAS_SINGLE_SUPPORT -Wall -I #{fftw_incdir}"].join(" ")
 end
 
-if $warnflags
+
+if $warnflags then
   $warnflags.slice!('-Wdeclaration-after-statement')
   $warnflags.slice!('-Wimplicit-function-declaration')
 end
-
 create_makefile("fftw/fftw")
