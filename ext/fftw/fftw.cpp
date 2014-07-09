13a34eee8cee6b8ac9e5a738818b56471a2bcfe6
Magdalen Berns <m.berns@thismagpie.com>
2014-07-09 03:58:28 +0100

    write in c++ and define class

--- ext/fftw/fftw.cpp
+++ ext/fftw/fftw.cpp
@@ -1,86 +1,86 @@
-#include <fftw3.h>
-#include "ruby.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include "nmatrix.h"
-#include "ruby_constants.h"
+#ifdef __FFTW_H__
+#include __FFTW_H__
+#endif
 #include "fftw.h"
 
-#ifdef cplusplus
-
-VALUE mFFTW;
-VALUE cFFTW;
-extern VALUE cNM;
-
-static void dealloc(void *nm)
+struct fftw_nm
 {
-  fftw_nm_free(FFTW::NMatrix->nm);
+  size_t size;
+  void *ptr;
+};
+
+/**
+ * Free up memory taken up ßfrom fftw_nm instance.
+ */
+static void
+fftw_nm_free(void *p)
+{
+  struct fftw_nm *ptr;
+
+  if (ptr->size > 0)
+    free(ptr->ptr);
 }
 
-static VALUE alloc(VALUE klass)
-{
-  NMatrix nm = alloc(sizeof(klass);
+/**
+ * Allocate memory taken up ßfrom fftw_nm instance.
+ */
 
-  return Data_Wrap_Struct(klass, NULL, dealloc, nm);
+static VALUE
+fftw_nm_alloc(VALUE klass)
+{
+  VALUE obj;
+  struct fftw_nm *ptr;
+  obj = Data_Make_Struct(klass,
+                         struct fftw_nm,
+                         NULL,
+                         fftw_nm_free,
+                         ptr);
+  ptr->size = 0;
+  ptr->ptr  = NULL;
+  return obj;
 }
 
-static VALUE length(VALUE self)
+static VALUE
+fftw_nm_init(VALUE self, VALUE size)
 {
-  FFTW_NMatrix * nm;
+  struct fftw_nm *ptr;
+  size_t requested = NUM2SIZET(size);
+  rb_const_get(rb_cObject, rb_intern("FFTW"));
+  Data_Get_Struct(self, struct fftw_nm, ptr);
 
-  Data_Get_Struct(self, FFTW_NMatrix, nm);
+  ptr->ptr = malloc(requested);
 
-  return INT2NUM(fftw_nm_get_length(nm));
-}
 
-typedef VALUE
-fftw_nm_float(VALUE nm)
-{
-  DENSE_STORAGE* s = NM_DENSE_STORAGE(nm);
-  nm_matrix* m = nm_fftw_matrix_float nm_fftw_float(fftw_nm_alloc( s->shape[0], s->shape[1] );
+  if (0 == requested)
+    rb_raise(rb_eArgError, "unable to allocate 0 bytes");
+  ptr->ptr = malloc(requested);
 
-  if (s->dtype != NM_FLOAT64) {
-    rb_raise(nm_eDataTypeError, "requires dtype of :float64 to convert double vector");
-  }
-  memcpy(m->data, s->elements, s->count);
-  return m;
-}
-  static VALUE fftw_nm_init(VALUE self, VALUE fftw_nm)
-  {
-    FFTW_NMatrix * nm;
-    void * data;
+  if (NULL == ptr->ptr)
+      rb_raise(rb_eNoMemError, "Unable to allocate %ld bytes", requested);
 
-    Check_Type(fftw_nm, T_FFTW_NMatrix);
-
-    Data_Get_Struct(self, FFTW_NMatrix, nm);
-
-    data = calloc(RSTRING_LEN(fftw_nm), sizeof(char));
-    memcpy(data, nmValuePtr(fftw_nm), fft_nm_LEN(fftw_nm));
-
-    fftw_nm_init(
-        string,
-        data,
-        sizeof(char),
-        RSTRING_LEN(fftw_nm));
+  ptr->size = requested;
 
   return self;
 }
-extern "C"
+
+static VALUE
+fftw_nm_release(VALUE self)
 {
-  void Init_nm_fftw()
-  {
-    /* Define module  */
-    mFFTW = rb_define_module("FFTW");
+  struct fftw_nm *ptr;
 
-    cNM = rb_define_class_under(mFFTW, "NMatrix", rb_cObject);
+  Data_Get_Struct(self, struct fftw_nm, ptr);
 
-    rb_define_alloc_func(cFFTW, alloc);
-    rb_define_alloc_func(cNM, alloc);
+  if (0 == ptr->size)
+      return self;
 
-    /* Call initialise function */
-    fftw_nm_init(cNM);
-    rb_define_method(cFFTW, "init", fftw_nm_init, 1);
-    rb_define_method(cFFTW, "nm_float", fftw_nm_float, 1);
-  }
+  ptr->size = 0;
+  free(ptr->ptr);
+
+  return self;
 }
-#endif
+void
+Init_fftw_nm (void)
+{
+  mFFTW = rb_define_module("FFTW");
+  cFFTW = rb_const_get(rb_cObject, rb_intern("FFTW"));
+}
