dd90c7aaf3bbbebc9dcdf2c28f94ab5f0d97b31d
Magdalen Berns <m.berns@thismagpie.com>
2014-07-10 10:54:08 +0100

    lib/ set up some fftw functions

--- lib/fftw.rb
+++ lib/fftw.rb
@@ -2,6 +2,31 @@ require 'nmatrix'
 require './lib/fftw'
 require 'colorize'
 
+class FFTW
+  def self.initialise()
+    raise ArgumentError, "need a block" if !block
+    define_method()
+  end
+  def self.release()
+    raise ArgumentError, "need a block" if !block
+    define_method()
+  end
+  def free(self)
+    raise ArgumentError, "need a block" if !block
+    define_method()
+  end
+  def method_missing(sym, *argv)
+
+      method_name = sym.to_s.downcase
+
+      if respond_to?(method_name) then
+          send(method_name, *argv)
+      else
+          raise NotImplementedError, "no such method #{method_name}"
+      end
+  end
+end
+
 if File.exist?("../lib/fftw/fftw.so")
   require 'fftw.so'
 end
---
+++ lib/fftw/fftw.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+class FFTW
+  describe "free" do
+  end
+  describe "initialise" do
+    FFTW.new("initialise")
+  end
+  describe "release" do
+  end
+end
