f88f213f308190b5ebb7b30d9613b90c105f122d
Magdalen Berns <m.berns@thismagpie.com>
2014-07-12 05:07:20 +0100

    fix compile errors

--- ext/fftw/fftw.cpp
+++ ext/fftw/fftw.cpp
@@ -3,14 +3,9 @@
 #include "ruby.h"
 #include <fftw3.h>
 #include <stdio.h>
+#include <fftw.h>
 #include <iostream>
-#include <fftw.hpp>
 #include "fftw_config.h"
-
-#ifdef __NMATRIX_H__
-  #include __NMATRIX_H__
-  VALUE cNMatrix;
-#endif
 
 VALUE mFFTW;
 VALUE cFFTW;
@@ -19,59 +14,72 @@ fftw_complex *fftw_complex_alloc(long n);
 
 using namespace std;
 
-/* The guru interface introduces one basic new nmatrix structure
-   fftw_iodim, that is used to specify sizes and strides for
-   multi-dimensional transforms and vectors
-   http://www.fftw.org/doc/Guru-vector-and-transform-sizes.html
-   The structure contains member functions of FFTW
-   */
-typedef struct fftw_nm{
-  long n;
-  VALUE size;
-  fftw_complex fc;
-  fftw_complex out;
-  VALUE nmatrix;
-} FFTW;
+#ifdef __cplusplus
+extern "C" {
+  typedef struct fftw
+  {
+    long n;
+    VALUE size;
+    fftw_complex fc;
+    fftw_complex out;
+    VALUE nmatrix;
+    VALUE ftw_r2c;
+  } FFTW;
 
-static void fftw_nm_free(void * nmatrix){
-  fftw_complex* fc;
+  static void fftw_nm_free(void * nmatrix){
+    fftw_complex* fc;
 
-  free(((FFTW *)nmatrix)->fc);
-  free(nmatrix);
-}
+    free(((FFTW *)nmatrix)->fc);
+    free(nmatrix);
+  }
 
-VALUE
-fftw_alloc(VALUE klass){
-  long n;
-  fftw_complex* fc;
+  VALUE
+  fftw_alloc(VALUE klass)
+  {
+    long n;
+    fftw_complex* fc;
 
-  FFTW *nmatrix;
-  nmatrix = ALLOC(FFTW);
-  nmatrix->n = n;
-  (FFTW *)nmatrix->fc = fc;
-  return Data_Wrap_Struct(klass, 0, free, nmatrix);
-}
+    FFTW *nmatrix;
+    nmatrix = ALLOC(FFTW);
 
-VALUE fftw_r2c(int argc, VALUE* argv, VALUE self) {
-  VALUE nmatrix, opts, size;
-  double *in;
-  fftw_complex out;
-  fftw_plan plan;
-  long n;
+    return Data_Wrap_Struct(klass, 0, free, nmatrix);
+  }
 
-  rb_scan_args(argc, argv, "11", &nmatrix, &opts);
-  in = (double *) fftw_malloc(sizeof(double) * n);  
-  /*out = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * (n/2+1)); */
-  /* TODO add plan */
-  return self;
-}
 
-void
-Init_fftw(void)
-{
-  long fftw_size(VALUE v);
-  rb_define_singleton_method(mFFTW, "fft_complex", fftw_complex, 1);
-  rb_define_singleton_method(mFFTW, "r2c", fftw_r2c, -1);
-  rb_define_singleton_method(mFFTW, "alloc", fftw_alloc, 0);
-  rb_define_singleton_method(mFFTW, "free", fftw_free, 0);
+  VALUE
+  fftw_r2c(int argc, VALUE* argv, VALUE self)
+  {
+    long n;
+    VALUE nmatrix, opts, size;
+    double *in;
+    fftw_complex *out;
+    fftw_plan plan;
+    int nm[]={2, 2};
+
+    rb_scan_args(argc, argv, "11", &nmatrix, &opts);
+
+  /* In place inpute */
+     in = (double*)malloc(sizeof(double)*(nm[0] * nm[1]));
+     out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) *  nm[0] * (nm[1]/2 + 1));
+
+
+     plan = fftw_plan_dft_r2c(2,
+                              nm,
+                              in,
+                              (fftw_complex*)in,
+                              FFTW_MEASURE
+                              );
+    /* TODO add plan */
+    return self;
+  }
+
+  void
+  Init_fftw(void)
+  {
+    rb_define_module("FFTW");
+    rb_define_singleton_method(mFFTW, "ifftw", fftw_complex, 1);
+    rb_define_singleton_method(mFFTW, "r2c", fftw_r2c, 1);
+    rb_define_singleton_method(mFFTW, "alloc", fftw_alloc, 0);
+    rb_define_singleton_method(mFFTW, "free", fftw_free, 0);
+  }
 }
