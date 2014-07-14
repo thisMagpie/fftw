18a4be47b876ad0884d536dfd077cc1c7c034f71
Magdalen Berns <m.berns@thismagpie.com>
2014-07-14 02:49:08 +0100

    comment blocks

--- ext/fftw/fftw.cpp
+++ ext/fftw/fftw.cpp
@@ -2,50 +2,68 @@
 #include <fftw3.h>
 #include <fftw.h>
 #include <stdio.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
 
-struct fftw {
-  long n;
-  VALUE size;
-  fftw_complex fc;
-  fftw_complex out;
-  VALUE nmatrix;
-  VALUE ftw_r2c;
-} FFTW;
+// Prototype for ruby to call
+void Init_fftw();
 
-VALUE
-fftw_r2c(int argc, VALUE* argv, VALUE self)
+VALUE mFFTW;
+VALUE cNMatrix;
+int i = 0;
+
+static VALUE fftw_nmatrix(int argc, VALUE* argv, VALUE self)
 {
-  long n;
-  VALUE nmatrix, opts, size;
+  VALUE in;
+  VALUE nmatrix, size;
+  rb_scan_args(argc, argv, "11", &nmatrix, &size);    // informs ruby that the method takes 1 mandatory and 1 optional argument, 
+                                                    // the values of which are stored in nmatrix and size.
+
+  if (NIL_P(size))         // if no size was given...
+    size = INT2NUM(10);  // use the default value
+
+  rb_iv_set(self, "in", in);
+
+  //double nm = NUM2DBL(rb_funcall(args, rb_intern("[]")) 2, 0, 0));
+
+ // rb_yield(Qnil);
+  printf("TEST\n");
+  return self;
+}
+
+  /* 
+
+VALUE fftw_r2c(VALUE self, VALUE nmatrix)
+{
+  int n;
+  VALUE size;
   double *in;
   fftw_complex *out;
   fftw_plan plan;
   int nm[]={2, 2};
 
-  rb_scan_args(argc, argv, "11", &nmatrix, &opts);
+  //In place inpute 
+  in = (double*)malloc(sizeof(double)*(nm[0] * nm[1]));
+  out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) *  nm[0] * (nm[1]/2 + 1));
 
-/* In place inpute */
-   in = (double*)malloc(sizeof(double)*(nm[0] * nm[1]));
-   out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) *  nm[0] * (nm[1]/2 + 1));
-
-
-  plan = fftw_plan_dft_r2c(self, nm, in, out, (const int) FFTW_ESTIMATE);
-  /* TODO add plan */
+   TODO add plan
+   Expected arguments
+   fftw_plan_dft_r2c(int rank, const int *n, double *in, fftw_complex *out, unsigned flags);
+  plan = fftw_plan_dft_r2c(...n, out, FFTW_ESTIMATE);
 
   fftw_execute(plan);
   free(in);
   fftw_destroy_plan(plan);
+
+
   return self;
 }
-
-void
-Init_fftw(void)
+*/
+/*
+void Init_fftw(void)
 {
-
-  VALUE cFFTW;
-  cFFTW = rb_define_class("FFTW", cFFTW);
-  rb_define_method(cFFTW, "ifft", fftw_complex, 0);
-  rb_define_method(cFFTW, "r2c", fftw_r2c, 1);
-  rb_define_method(cFFTW, "alloc", fftw_malloc, 0);
-  rb_define_method(cFFTW, "free", fftw_free, 0);
-}
+  mFFTW = rb_define_module("FFTW");
+  cNMatrix = rb_define_class("NMatrix",mFFTW);
+  rb_define_singleton_method(cNMatrix, "nmatrix", fftw_nmatrix, 0);
+}*/
