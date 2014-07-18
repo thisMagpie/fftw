6ef33eb2688802d1892734aff7384f881f50afb0
Magdalen Berns <m.berns@thismagpie.com>
2014-07-19 00:07:50 +0100

    spec remove comparison with uninited constant

--- spec/fftw_spec.rb
+++ spec/fftw_spec.rb
@@ -6,6 +6,5 @@ describe FFTW, "nmatrix" do
   it "creates an nmatrix object" do
     n = NMatrix.new([2,2], [0,1,2,3], dtype: :int64)
     expect(n.dtype).to eq(:int64)
-    expect(FFTW::NMatrix.new([2,2])).to eq(n)
   end
 end
