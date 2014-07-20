2ca9488ab475dad585bc592b03428ddf3707a593
Magdalen Berns <m.berns@thismagpie.com>
2014-07-20 08:46:40 +0100

    add nmatrix spec

--- spec/fftw_spec.rb
+++ spec/fftw_spec.rb
@@ -4,7 +4,21 @@ require 'nmatrix'
 
 describe FFTW, "nmatrix" do
   it "creates an nmatrix object" do
-    n = NMatrix.new([2,2], [0,1,2,3], dtype: :int64)
+    n = NMatrix.new([2,2], dtype: :int64)
     expect(n.dtype).to eq(:int64)
+  end
+
+ it "fills dense with individual assignments" do
+    n = NMatrix.new([2,2],
+                    [-3.105036184601418e+231,
+                     -1.730600617294159e-77,
+                     1.0e-323,
+                     2.83811411759045e-309],
+                     dtype: :float64)
+    puts n
+    expect(n[0,0]).to eq(-3.105036184601418e+231)
+    expect(n[0,1]).to eq(-1.730600617294159e-77)
+    expect(n[1,0]).to eq(1.0e-323)
+    expect(n[1,1]).to eq(2.83811411759045e-309)
   end
 end
