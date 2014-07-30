ba7e7e96d10d6f472039ab5d85aecfb8ce3f579e
Magdalen Berns <m.berns@thismagpie.com>
2014-07-30 05:19:02 +0100

    move requires to spec helper

--- spec/fftw_spec.rb
+++ spec/fftw_spec.rb
@@ -1,25 +1,24 @@
 require 'spec_helper'
-require 'fftw'
-require 'nmatrix'
-require 'spec_helper'
 
-describe FFTW, "r2c" do
-  it "creates an nmatrix object" do
-    n = NMatrix.new([2,2], dtype: :int64)
-    expect(n.dtype).to eq(:int64)
-  end
+describe FFTW do
+  describe "r2c" do
+    it "creates an nmatrix object" do
+      n = NMatrix.new([2,2], dtype: :int64)
+      expect(n.dtype).to eq(:int64)
+    end
 
- it "fills dense with individual assignments" do
-    n = NMatrix.new([2,2],
-                    [-3.105036184601418e+231,
-                     -1.730600617294159e-77,
-                     1.0e-323,
-                     2.83811411759045e-309],
-                     dtype: :float64)
-    puts n
-    expect(n[0,0]).to eq(-3.105036184601418e+231)
-    expect(n[0,1]).to eq(-1.730600617294159e-77)
-    expect(n[1,0]).to eq(1.0e-323)
-    expect(n[1,1]).to eq(2.83811411759045e-309)
+   it "fills dense with individual assignments" do
+      n = NMatrix.new([2,2],
+                      [-3.105036184601418e+231,
+                       -1.730600617294159e-77,
+                       1.0e-323,
+                       2.83811411759045e-309],
+                       dtype: :float64)
+      puts n
+      expect(n[0,0]).to eq(-3.105036184601418e+231)
+      expect(n[0,1]).to eq(-1.730600617294159e-77)
+      expect(n[1,0]).to eq(1.0e-323)
+      expect(n[1,1]).to eq(2.83811411759045e-309)
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,7 @@ require 'bundler/setup'
 Bundler.setup
 require 'fftw'
 require 'rspec'
+require 'nmatrix'
 require './lib/fftw'
+require 'rspec/core'
+require 'rspec/core/formatters/base_formatter'
