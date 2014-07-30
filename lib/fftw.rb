a5997cf8b9cf2616326661f17f43b7b2cf59783e
Magdalen Berns <m.berns@thismagpie.com>
2014-07-30 03:56:30 +0100

    lib: fix missing method

--- lib/fftw.rb
+++ lib/fftw.rb
@@ -10,16 +10,17 @@ if File.exist?("../ext/fftw/fftw.o")
 end
 
 module FFTW
-  class FFTW::NMatrix
+  class fftw
     def self.r2c(sym, *argv)
       define_singleton_method()
     end
-    def method_missing(sym, *argv)
-      method_name = sym.to_s.downcase
-      if respond_to?(method_name) then
-        send(method_name, *argv)
+    def missing(sym, *argv)
+      define_singleton_method()
+      method = sym.to_s.downcase
+      if respond_to?(method) then
+        send(method, *argv)
       else
-        raise NotImplementedError, "no such method #{method_name}"
+        raise NotImplementedError, "no such method #{method}"
       end
     end
   end
