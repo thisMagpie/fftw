157db07143563dca0b7a9f5db50c1a054d830a0f
Magdalen Berns <m.berns@thismagpie.com>
2014-07-31 22:29:32 +0100

    add broken spec to check log on travis

--- spec/fftw_spec.rb
+++ spec/fftw_spec.rb
@@ -1,24 +1,32 @@
 require 'spec_helper'
 
 describe FFTW do
+  attr_accessor :size
+  def init(nm={})
+    self.size = nm[:size]
+  end
   describe "r2c" do
-    it "creates an nmatrix object" do
+    it "creates an NMatrix object" do
       n = NMatrix.new([2,2], dtype: :int64)
       expect(n.dtype).to eq(:int64)
     end
-
-   it "fills dense with individual assignments" do
+    it "fills dense with individual assignments" do
       n = NMatrix.new([2,2],
                       [-3.105036184601418e+231,
                        -1.730600617294159e-77,
-                       1.0e-323,
-                       2.83811411759045e-309],
-                       dtype: :float64)
-      puts n
+                        1.0e-323,
+                        2.83811411759045e-309],
+                        dtype: :float64)
       expect(n[0,0]).to eq(-3.105036184601418e+231)
       expect(n[0,1]).to eq(-1.730600617294159e-77)
       expect(n[1,0]).to eq(1.0e-323)
       expect(n[1,1]).to eq(2.83811411759045e-309)
+    end
+  end
+  describe "new" do
+    it "creates a new FFTW object" do
+      fftw = FFTW.new
+      pending
     end
   end
 end
