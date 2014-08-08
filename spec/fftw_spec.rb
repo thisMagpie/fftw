fde451d42cd1e417d808eed50de222e0cc7f4613
Magdalen Berns <m.berns@thismagpie.com>
2014-08-08 22:58:30 +0100

    use absolute paths in lib and spec_helper requires

--- spec/fftw_spec.rb
+++ spec/fftw_spec.rb
@@ -1,15 +1,20 @@
-require 'spec_helper'
+#!/usr/bin/env ruby
+require File.expand_path(File.dirname(__FILE__)+"/spec_helper.rb")
 
-describe FFTW do
+RSpec.describe FFTW do
+  it "Creates a new FFTW  instance" do
+    fftw = FFTW::FFTW.new()
+    expect(fftw).to eq(fftw)
+  end
   describe "v" do
-    it "creates an FFTW::FFTW::v object"
+    it "Creates an FFTW::FFTW.v object"
   end
   describe "r2c" do
     it "creates an NMatrix object" do
       n = NMatrix.new([2,2], dtype: :int64)
       expect(n.dtype).to eq(:int64)
     end
-    it "fills dense with individual assignments" do
+    it "Fills dense with individual assignments" do
       n = NMatrix.new([2,2],
                       [-3.105036184601418e+231,
                        -1.730600617294159e-77,
@@ -21,14 +26,8 @@ describe FFTW do
       expect(n[1,0]).to eq(1.0e-323)
       expect(n[1,1]).to eq(2.83811411759045e-309)
     end
-    it "creates a new FFTW  instance" do
-      fftw = FFTW::FFTW.new()
-      expect(fftw).to eq(fftw)
-    end
-    it "creates a new FFTW::FFTW.r2c object and takes an NMatrix as its argument" do
-      n = NMatrix.new([2,2], dtype: :int64)
-      fftw = FFTW::FFTW.r2c(n)
-      expect(fftw).to eq(fftw)
+    it "Creates a new FFTW::FFTW.r2c object and takes an NMatrix as its argument" do
+      fftw = FFTW::FFTW.r2c([3.10, 1.73, 1.04, 2.83])
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+$:.unshift(File.dirname(__FILE__)+"/../lib")
 require 'bundler/setup'
 Bundler.setup
 require 'fftw'
