220f1fd9b0937165671c466a2dee915940c797a3
Magdalen Berns <m.berns@thismagpie.com>
2014-08-09 18:31:51 +0100

    lib/ remove module definition

--- lib/fftw.rb
+++ lib/fftw.rb
@@ -11,23 +11,22 @@ def check_exists(str, req)
 end
 check_exists("./lib/fftw/fftw.so",'fftw.so')
 
-module FFTW
-  class FFTW
-    def self.v(sym, *argv)
-      define_singleton_method()
-    end
-    def self.r2c(nmatrix)
-      define_singleton_method()
-      return nmatrix
-    end
-    def missing(sym, *argv)
-      define_singleton_method()
-      method = sym.to_s.downcase
-      if respond_to?(method) then
-        send(method, *argv)
-      else
-        raise NotImplementedError, "no such method #{method}"
-      end
+class FFTW
+  def self.v(sym, *argv)
+    define_singleton_method()
+  end
+  def self.r2c(nmatrix)
+    define_singleton_method()
+  end
+  def missing(sym, *argv)
+    define_singleton_method()
+    method = sym.to_s.downcase
+    if respond_to?(method) then
+      send(method, *argv)
+    else
+      raise NotImplementedError, "no such method #{method}"
     end
   end
 end
+
+
