8b11ba541e08f7e0ed912797df47b51c6035d919
Magdalen Berns <m.berns@thismagpie.com>
2014-08-12 17:10:40 +0100

    lib/ add some documentation

--- lib/fftw.rb
+++ lib/fftw.rb
@@ -1,14 +1,17 @@
 require 'nmatrix'
 require 'colorize'
 
+# Check the library .so file exists
+# @param str 
+#            the path to the file
+# @param     the file to be required
+# @return 	 void
 def check_exists(str, req)
   if File.exist?(str)
-  	require str
+  	require str #=> Only require './lib/fftw/fftw.so' if found first
     puts "#{str} found!".green #=>
   else
     puts "#{str} not found!".red #=>
   end
 end
 check_exists("./lib/fftw/fftw.so",'fftw.so')
-
-
