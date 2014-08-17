03b8aaec78889f59cd996ef98aab3e7d0c4a056e
Magdalen Berns <m.berns@thismagpie.com>
2014-08-17 03:08:17 +0100

    spec: add fftw[] check
    This was not working before but not it is fixed so put a
    check to ensure it stays so

--- spec/fftw_spec.rb
+++ spec/fftw_spec.rb
@@ -18,16 +18,19 @@ describe FFTW do
     expect(n[1,0]).to eq(1.0e-323)
     expect(n[1,1]).to eq(2.83811411759045e-309)
   end
-  it "Creates a new FFTW.r2c object and takes an NMatrix as its argument" do
+  it "Creates a new FFTW.r2c object and takes an a 1D NMatrix as its argument" do
     n = NMatrix.new([4], [3.10, 1.73, 1.04, 2.83])
     puts n
     fftw = FFTW.r2c(n)
     expect(fftw).to eq(fftw)
   end
-  it "Creates a new FFTW.r2c object and takes a 1D NMatrix of integers as its argument" do
-    n = NMatrix.new([4], [1, 4, 5, 7])
+  it "Creates a new FFTW.r2c object which takes a 1D NMatrix of integers and checks it can express its indices" do
+    n = NMatrix.new([3], [4, 5, 7])
     puts n
     fftw = FFTW.r2c(n)
-    expect(fftw).to eq(fftw)
+    for i in 0..2 do
+      puts "INDEX: #{i}"
+      expect(fftw[i]).to eq(fftw[i])
+    end
   end
 end
