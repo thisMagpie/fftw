532c152365dbed3a3b6f3486e04ddc21e4d1003c
Magdalen Berns <m.berns@thismagpie.com>
2014-08-29 19:15:44 +0100

    specs fix all rspecs

--- spec/fftw_spec.rb
+++ spec/fftw_spec.rb
@@ -14,13 +14,11 @@ describe FFTW do
   end
 
   it "creates an NMatrix object" do
-    skip
     n = NMatrix.new([2,2], dtype: :int64)
     expect(n.dtype).to eq(:int64)
   end
 
   it "Fills dense with individual assignments" do
-    skip
     n = NMatrix.new([2,2],
                     [-3.105036184601418e+231,
                      -1.730600617294159e-77,
@@ -33,46 +31,38 @@ describe FFTW do
     expect(n[1,1]).to eq(2.83811411759045e-309)
   end
 
-  it "Creates a new FFTW.Z object and takes an a 1D NMatrix as its argument" do
-    skip
-    n = NMatrix.new([4], [3.10, 1.73, 1.04, 2.83])
-    r = FFTW.Z(n)
-    # Need to provide an expected value for Z
-  end
-
   it "Creates a new FFTW.r2c object which takes a 1D NMatrix of integers and checks it can express its indices" do
-    skip
     n = NMatrix.new([3], [4, 5, 7])
-    puts n
-    fftw = FFTW.r2c_one(n)
+    Z = NMatrix.zeros([3], dtype: :complex128)
+    fftw = FFTW.r2c_one(n, Z)
     for i in 0..2 do
-      puts "INDEX: #{i}"
+      puts "INDEX: #{i}= #{fftw[i]}"
       expect(fftw[i]).to eq(fftw[i])
     end
   end
   
   it "Checks NMatrix in FFTW.r2c_one and NMatrix itself have the same value for shape" do
-    skip
     n = NMatrix.new([6], [-3.10,
                           -1.73,
                            1.0,
                            2.84,
                            56.42,
                            -32.1])
-    fftw = FFTW.r2c_one(n)
+    Z = NMatrix.zeros([6], dtype: :complex128)
+    fftw = FFTW.r2c_one(n, Z)
     expect(n.shape).to eq(fftw.shape)
     puts "#{n.shape} == #{fftw.shape}"
   end
 
   it "Checks NMatrix in FFTW.r2c_one and NMatrix itself have the same value for size" do
-    skip
     n = NMatrix.new([6], [-3.10,
                           -1.73,
                            1.0,
                            2.84,
                            56.42,
                            -32.1])
-    fftw = FFTW.r2c_one(n)
+    Z = NMatrix.zeros([6], dtype: :complex128)
+    fftw = FFTW.r2c_one(n, Z)
     expect(n.size).to eq(fftw.size)
     puts "Size: #{n.size} == #{fftw.size}"
   end
